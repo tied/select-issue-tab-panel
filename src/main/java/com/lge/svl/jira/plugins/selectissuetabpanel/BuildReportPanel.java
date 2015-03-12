483abf10f14e5eeffafd3cbab01818d647eebd3b
Sunjoo Park <allessunjoo.park@lge.com>
2015-03-12 15:54:51 +0900

    Create 'Buid Report' Panel

---
+++ src/main/java/com/lge/svl/jira/plugins/selectissuetabpanel/BuildReportPanel.java
@@ -0,0 +1,52 @@
+package com.lge.svl.jira.plugins.selectissuetabpanel;
+
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.comments.Comment;
+import com.atlassian.jira.issue.comments.CommentManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.atlassian.jira.plugin.issuetabpanel.AbstractIssueTabPanel;
+import com.atlassian.jira.plugin.issuetabpanel.IssueTabPanel;
+import com.atlassian.jira.issue.tabpanels.GenericMessageAction;
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.crowd.embedded.api.User;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BuildReportPanel extends AbstractIssueTabPanel implements IssueTabPanel
+{
+    private static final Logger log = LoggerFactory.getLogger(BuildReportPanel.class);
+    private static final String buildReportUser = "Jira2 Svl";
+
+    public List getActions(Issue issue, User remoteUser) {
+        CommentManager commentManager = ComponentAccessor.getCommentManager();
+        CommitMessageAction commitMessageAction = new CommitMessageAction("Commit Message");
+
+        List<Comment> comments = commentManager.getComments(issue);
+
+        for(Comment c : comments){
+            if ( c.getAuthorApplicationUser().getUsername().equals(buildReportUser)) {
+                commitMessageAction.addComment(c);
+            }
+        }
+        return Collections.singletonList(commitMessageAction);
+    }
+
+    public boolean showPanel(Issue issue, User remoteUser)
+    {
+        CommentManager commentManager = ComponentAccessor.getCommentManager();
+
+        boolean displayPanel = false;
+
+        List<Comment> comments = commentManager.getComments(issue);
+        for(Comment c : comments){
+            if ( c.getAuthorApplicationUser().getUsername().equals(buildReportUser)) {
+                displayPanel = true;
+                break;
+            }
+        }
+
+        return displayPanel;
+    }
+}
---
+++ src/test/java/ut/com/lge/svl/jira/plugins/selectissuetabpanel/BuildReportPanelTest.java
@@ -0,0 +1,34 @@
+package ut.com.lge.svl.jira.plugins.selectissuetabpanel;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.lge.svl.jira.plugins.selectissuetabpanel.BuildReportPanel;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class BuildReportPanelTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //BuildReportPanel testClass = new BuildReportPanel();
+
+        throw new Exception("BuildReportPanel has no tests!");
+
+    }
+
+}
